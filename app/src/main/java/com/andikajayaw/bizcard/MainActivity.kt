0f6f0869099bbaf3a7c55676f35d146059afb15d
andikajayaw <andikajayawiguna@gmail.com>
2022-01-12 23:25:48 +0800

    create card use properties w/ modifier

--- app/src/main/java/com/andikajayaw/bizcard/MainActivity.kt
+++ app/src/main/java/com/andikajayaw/bizcard/MainActivity.kt
@@ -3,14 +3,18 @@ package com.andikajayaw.bizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.andikajayaw.bizcard.ui.theme.BizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,8 +33,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
-    Surface(modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
-        
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()) {
+        Card(modifier = Modifier.width(200.dp).height(390.dp).padding(12.dp),
+            shape= RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = 4.dp) {
+        }
     }
 }
 
